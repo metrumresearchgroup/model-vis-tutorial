0ca6cd4d500725f50de6623406dd5251261a3215
Kyle Baron <kyleb@metrumrg.com>
2017-03-17 22:23:29 -0400

    rebuild

---
+++ pkgSetup.R
@@ -0,0 +1,16 @@
+if(!file.exists("lib")) dir.create("lib")
+.libPaths("lib")
+
+
+if(FALSE) {
+  install.packages(c("dplyr", "Rcpp", "BH", "RcppArmadillo", 
+                "shiny", "shinydashboard", "ggplot2", "dmutate","tidyr"))
+
+  install.packages("flexdashboard")
+  install.packages("mrgsolve")
+}
+
+
+
+
+
